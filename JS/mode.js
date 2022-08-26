b30f6a3848b52d9577612e7dcaa0a6c0fbc81c3a
anamileoh <ana.rojiita@gmail.com>
2022-08-26 11:12:23 -0500

    light mode

---
+++ JS/mode.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', init)
+
+function init() {
+    const buttonLite = document.getElementById("button-light");
+    buttonLite.addEventListener('click', changeLight)
+}
+
+function changeLight() {
+document.querySelector("body").classList.toggle("body-change-mode");
+document.querySelector("a").classList.toggle("a-change-mode");
+// document.querySelector(".main-index").classList.toggle("main-change-mode");
+document.querySelector(".nav-links").querySelectorAll("li a").forEach(navElement => {
+
+navElement.classList.toggle("nav-change-mode")}
+)
+
+
+let variable = document.querySelector(".main-info");
+if (variable) {
+    variable.classList.toggle("main-change-mode");
+
+
+}
+}
