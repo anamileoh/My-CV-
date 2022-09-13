4109a8a971e1055165289cd54479293f3fe17c86
anamileoh <ana.rojiita@gmail.com>
2022-09-13 16:12:30 -0500

    Dont know

--- JS/mode.js
+++ JS/mode.js
@@ -6,20 +6,18 @@ function init() {
 }
 
 function changeLight() {
-document.querySelector("body").classList.toggle("body-change-mode");
-document.querySelector("a").classList.toggle("a-change-mode");
+    document.querySelector("body").classList.toggle("body-change-mode");
+    document.querySelector("a").classList.toggle("a-change-mode");
 
-// document.querySelector(".main-index").classList.toggle("main-change-mode");
-document.querySelector(".nav-links").querySelectorAll("li a").forEach(navElement => {
-
-navElement.classList.toggle("nav-change-mode")}
-)
-
-
-let variable = document.querySelector(".main-info");
-if (variable) {
-    variable.classList.toggle("main-change-mode");
+    // document.querySelector(".main-index").classList.toggle("main-change-mode");
+    document.querySelector(".nav-links").querySelectorAll("li a")
+        .forEach(navElement => {
+            navElement.classList.toggle("nav-change-mode")
+        })
 
 
-}
+    let variable = document.querySelector(".main-info");
+    if (variable) {
+        variable.classList.toggle("main-change-mode");
+    }
 }
