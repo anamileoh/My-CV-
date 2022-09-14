64647ab21758d87496e122387e5ce187abe083cd
anamileoh <ana.rojiita@gmail.com>
2022-09-13 22:14:19 -0500

    almost there

--- JS/mode.js
+++ JS/mode.js
@@ -9,11 +9,36 @@ function changeLight() {
     document.querySelector("body").classList.toggle("body-change-mode");
     document.querySelector("a").classList.toggle("a-change-mode");
 
-    // document.querySelector(".main-index").classList.toggle("main-change-mode");
     document.querySelector(".nav-links").querySelectorAll("li a")
         .forEach(navElement => {
             navElement.classList.toggle("nav-change-mode")
-        })
+        });
+
+
+    document.querySelectorAll(".caja-info").forEach(caja => {
+        caja.classList.toggle("caja-info-change-mode");
+    })
+
+    document.querySelectorAll(".logo-wwc").forEach(logoW => {
+        logoW.classList.toggle("logo-wwc-change-mode");
+    })
+
+    if (document.querySelector(".logo-red")) {
+        document.querySelector(".logo-red").classList.toggle("logo-red-change-mode");
+
+    }
+    if (document.querySelector(".joke-box")) {
+
+        document.querySelector(".joke-box").classList.toggle("joke-box-change-mode");
+        document.querySelector(".joke-box").querySelectorAll("div")
+        .forEach(div => {
+            div.classList.toggle("div-joke-change-mode")
+        });
+
+    }
+
+    
+
 
 
     let variable = document.querySelector(".main-info");
